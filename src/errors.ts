8728d908dd244693f540f055b6b1301b6a1bc9d1
Peter Hickling <footballcraztpete@gmail.com>
2020-03-19 11:43:10 +0000

    Default error if error object is not the correct shape

--- src/errors.ts
+++ src/errors.ts
@@ -2,20 +2,22 @@ import boom from 'boom'
 import { logger } from 'logger'
 
 export const errorHandler = (err, req, res, next) => {
-    logger.error(err)
+  logger.error(err)
 
-    res.status(err.output.statusCode).json(err.output.payload)
+  res
+    .status(err?.output?.statusCode || 500)
+    .json(err?.output?.payload || 'Internal server error')
 }
 
 export const notFoundHandler = (req, res, next) => {
-    next(boom.notFound('Not found'))
+  next(boom.notFound('Not found'))
 }
 
 export const internalErrorWrapper = func => async (req, res, next) => {
-    try {
-        await func(req, res, next)
-        logger.info(`success.`)
-    } catch (error) {
-        next(boom.internal(error))
-    }
+  try {
+    await func(req, res, next)
+    logger.info(`success.`)
+  } catch (error) {
+    next(boom.internal(error))
+  }
 }
